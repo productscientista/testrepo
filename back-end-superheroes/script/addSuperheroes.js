0de9de0d4787ed69bc727ebb9479059a7b1037d0
productscientista <wakanda@admins-iMac.local>
2017-04-05 18:35:50 +0200

    First commit

---
+++ back-end-superheroes/script/addSuperheroes.js
@@ -0,0 +1,1 @@
+ds.Superhero.remove(); //clean the database from all former Superhero generatednew ds.Superhero({name:"Batman"}).save();new ds.Superhero({name:"Wolverine"}).save();var favoriteSupehero = new ds.Superhero({name:"Wonder Woman"});favoriteSupehero.save();ds.Superhero.all();
---
+++ web/gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  proxy = require('http-proxy-middleware'),
+  minimist = require('minimist'),
+  useref = require('gulp-useref'),
+  gulpIf = require('gulp-if'),
+  uglify = require('gulp-uglify'),
+  clean = require('gulp-clean'),
+  cssnano = require('gulp-cssnano'),
+  gulpOpen = require('gulp-open');
+
+var defaultOptions = {
+  serverUrl: 'http://127.0.0.1:8081',
+  port: 8000,
+  livereloadPort: 35729,
+  paths: [
+    'app/**',
+    'index.html'
+  ]
+};
+
+var config = minimist(process.argv.slice(2), {
+  default: defaultOptions,
+  string: ['serverUrl', 'port', 'livereload']
+});
+
+
+['serverUrl', 'port', 'livereloadPort'].forEach(function (opt) {
+  if (!config[opt]) {
+    config[opt] = defaultOptions[opt];
+  }
+});
+
+gulp.task('watch', function () {
+  gulp.watch(config.paths, ['reload']);
+});
+
+gulp.task('reload', function () {
+  gulp.src(config.paths).pipe(connect.reload());
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: ['.'],
+    port: config.port,
+    livereload: {
+      port: config.livereloadPort
+    },
+    middleware: function (conn, opt) {
+      return [
+        proxy('/rest', {
+          target: config.serverUrl,
+          changeOrigin: false,
+          ws: true
+        })
+      ];
+    }
+  });
+
+  gulp.src('index.html').pipe(gulpOpen({
+    uri: 'http://localhost:' + config.port
+  }));
+});
+
+gulp.task('build', ['clean'], function () {
+  gulp.src('assets/img/**/*')
+    .pipe(gulp.dest('dist/assets/img'));
+
+  return gulp.src('*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('dist', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('serve', ['connect', 'watch']);
+
+gulp.task('default', ['serve']);
